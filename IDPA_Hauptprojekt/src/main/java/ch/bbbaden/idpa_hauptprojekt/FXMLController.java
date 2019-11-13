8e995455a46ebfc9e61b3860b83d18abe84d8d57
Feedmon <dennis.beerli@stud.bbbaden.ch>
2019-11-13 14:12:56 +0100

    Login_Vorbereitung

--- IDPA_Hauptprojekt/src/main/java/ch/bbbaden/idpa_hauptprojekt/FXMLController.java
+++ IDPA_Hauptprojekt/src/main/java/ch/bbbaden/idpa_hauptprojekt/FXMLController.java
@@ -1,25 +1,62 @@
 package ch.bbbaden.idpa_hauptprojekt;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class FXMLController implements Initializable {
-    
+
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button registerButton;
     @FXML
     private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+    @FXML
+    private void handleLogin(ActionEvent event) throws IOException {
+        Stage window = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Login.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+        window.setTitle("Eingeloggt");
+        window.setScene(scene);
+
+        window.show();
+
+        Stage stage = (Stage) loginButton.getScene().getWindow();
+        stage.close();
+    }
+
+    @FXML
+    private void handleRegister(ActionEvent event) throws IOException {
+        Stage window = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/FXMLRegister.fxml"));
+
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+        window.setTitle("Registrierung");
+        window.setScene(scene);
+
+        window.show();
+
+        Stage stage = (Stage) loginButton.getScene().getWindow();
+        stage.close();
+    }
 }
