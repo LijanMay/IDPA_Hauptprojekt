f05a3452d311e3d07603464629a4b1bf1624bb75
Natalie.Rytz <natalie.rytz@stud.bbbaden.ch>
2019-11-20 13:05:57 +0100

    Update LoginController.java

--- IDPA_Hauptprojekt/src/main/java/ch/bbbaden/idpa_hauptprojekt/LoginController.java
+++ IDPA_Hauptprojekt/src/main/java/ch/bbbaden/idpa_hauptprojekt/LoginController.java
@@ -22,6 +22,7 @@ public class LoginController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        System.out.println("test");
     }    
     
 }
