8d3bab923dd4e37d6e2169fab27789e50aa25031
Feedmon <dennis.beerli@stud.bbbaden.ch>
2019-11-27 13:07:32 +0100

    n

--- IDPA_Hauptprojekt/src/main/java/ch/bbbaden/idpa_hauptprojekt/LoggedInSchuelerController.java
+++ IDPA_Hauptprojekt/src/main/java/ch/bbbaden/idpa_hauptprojekt/LoggedInSchuelerController.java
@@ -7,7 +7,11 @@ package ch.bbbaden.idpa_hauptprojekt;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 
 /**
  * FXML Controller class
@@ -16,12 +20,28 @@ import javafx.fxml.Initializable;
  */
 public class LoggedInSchuelerController implements Initializable {
 
+    @FXML
+    private ListView<String> listQuizes;
+    @FXML
+    private Button startQuiz;
+    @FXML
+    private Button insertQuiz;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
     }    
+
+    @FXML
+    private void handleStartQuiz(ActionEvent event) {
+    }
+
+    @FXML
+    private void handleInsertQuiz(ActionEvent event) {
+    }
     
 }
