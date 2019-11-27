851f8b9cf2f0ebb8dfbc7fda043160d7d8e34cbc
Natalie.Rytz <natalie.rytz@stud.bbbaden.ch>
2019-11-27 14:00:43 +0100

    s

---
+++ IDPA_Hauptprojekt/src/main/java/ch/bbbaden/idpa_hauptprojekt/NewQuestionTeacherController.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.bbbaden.idpa_hauptprojekt;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author natalie1703
+ */
+public class NewQuestionTeacherController implements Initializable {
+
+    @FXML
+    private Button createQuestion;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void hanldeCreateQuestion(ActionEvent event) {
+        // Frage mit eigenem Neuen Fenster erstellen mit for loop für anzahl mulitple choice fragen etc
+        
+    }
+    
+}
