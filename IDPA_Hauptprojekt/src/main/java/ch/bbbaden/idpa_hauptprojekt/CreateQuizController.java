dc6a5bb817ab45880f17fe226ad0f3fd87b8cef8
Max <max.stoll@stud.bbbaden.ch>
2019-12-11 14:15:33 +0100

    Added functions to CreateQuiz

--- IDPA_Hauptprojekt/src/main/java/ch/bbbaden/idpa_hauptprojekt/CreateQuizController.java
+++ IDPA_Hauptprojekt/src/main/java/ch/bbbaden/idpa_hauptprojekt/CreateQuizController.java
@@ -6,7 +6,10 @@
 package ch.bbbaden.idpa_hauptprojekt;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -51,10 +54,71 @@ public class CreateQuizController implements Initializable {
     @FXML
     private Button BDone;
 //   Done
+    @FXML
+    private Button BFAddAll;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        LVTG.getItems().add("Test");
+        LVTG.getItems().add("Test2");
+        LVTG.getItems().add("Test3");
+        LVTG.getItems().add("Test4");
+    }
+
+//    adds question to ListViewFragen
+    @FXML
+    private void addTG(ActionEvent event) {
+        try {
+            Object selected = LVTG.getSelectionModel().getSelectedItem();
+            moveTo(LVTG, LVF, selected);
+        } catch (Exception e) {
+            System.out.println("no TG selected");
+        }
+    }
+
+//    adds question to ListViewDeineFragen
+    @FXML
+    private void addFrage(ActionEvent event) {
+        try {
+            Object selected = LVF.getSelectionModel().getSelectedItem();
+            moveTo(LVF, LVDF, selected);
+        } catch (Exception e) {
+            System.out.println("nothing selected");
+        }
+    }
+
+//    adds all questions from LVF to LVDF
+    @FXML
+    private void addAlleFragen(ActionEvent event) {
+        try {
+            ObservableList<Object> questions;
+            questions = LVF.getSelectionModel().getSelectedItems();
+            for(Object o : questions){
+                moveTo(LVF, LVDF, o);
+            }
+        } catch (Exception e) {
+            System.out.println("no questions to add");
+        }
+    }
+
+//    deletes question from LVDF
+    @FXML
+    private void deleteFrage(ActionEvent event) {
+        try {
+            Object selected = LVDF.getSelectionModel().getSelectedItem();
+            moveTo(LVDF, LVF, selected);
+        } catch (Exception e) {
+            System.out.println("nothing to remove");
+        }
+    }
+
+//    move an entry from one listview to another
+    private void moveTo(ListView lv1, ListView lv2, Object o) {
+        if (o != null) {
+            lv2.getItems().add(o);
+            lv1.getItems().remove(o);
+        }
+
     }
 
 }
