9cfee85a2b561db82e60d33a7268b216c73a0b09
WoodPeckker <37664986+WoodPeckker@users.noreply.github.com>
2020-01-08 12:47:37 +0100

    apassige

--- IDPA_Hauptprojekt/src/main/java/ch/bbbaden/idpa_hauptprojekt/FXMLController.java
+++ IDPA_Hauptprojekt/src/main/java/ch/bbbaden/idpa_hauptprojekt/FXMLController.java
@@ -1,8 +1,5 @@
 package ch.bbbaden.idpa_hauptprojekt;
 
-import ch.bbbaden.idpa_hauptprojekt.Datatransfer.Database;
-import ch.bbbaden.idpa_hauptprojekt.Datatransfer.Textfile;
-import static java.awt.Color.red;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
@@ -15,16 +12,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.input.DragEvent;
 import javafx.scene.paint.Color;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javax.swing.JOptionPane;
 
